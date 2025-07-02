261063be9967354780357f332841c55208038f21
Michael228 <michaeltest228@gmail.com>
2025-07-02 13:17:14 +0300

    Alpha 0.1.1

--- wm.py
+++ wm.py
@@ -8,16 +8,16 @@ import time
 import threading
 from datetime import datetime
 
-MODKEY = X.Mod1Mask
+MODKEY = int(X.Mod1Mask)
 KEY_J = XK.string_to_keysym('j')
 KEY_K = XK.string_to_keysym('k')
 KEY_ENTER = XK.string_to_keysym('Return')
 KEY_Q = XK.string_to_keysym('q')
 
 MENU_HEIGHT = 28
-MENU_BG = 0x222222
-MENU_FG = 0xffffff
-MENU_HL = 0x4444ff
+MENU_BG = int(0x222222)
+MENU_FG = int(0xffffff)
+MENU_HL = int(0x4444ff)
 MENU_ITEMS = [
     ('XTerm', ['xterm']),
     ('Firefox', ['firefox']),
@@ -44,10 +44,10 @@ class WM:
     def grab_keys(self):
         for keysym in [KEY_J, KEY_K, KEY_ENTER, KEY_Q]:
             keycode = self.disp.keysym_to_keycode(keysym)
-            self.root.grab_key(keycode, MODKEY, 1, X.GrabModeAsync, X.GrabModeAsync)
+            self.root.grab_key(int(keycode), MODKEY, 1, X.GrabModeAsync, X.GrabModeAsync)
 
     def create_menu(self):
-        sw = self.root.get_geometry().width
+        sw = int(self.root.get_geometry().width)
         self.menu_win = self.root.create_window(
             0, 0, sw, MENU_HEIGHT, 0,
             self.disp.screen().root_depth,
@@ -64,11 +64,11 @@ class WM:
         self.menu_win.clear_area()
         x = 10
         for i, (label, _) in enumerate(MENU_ITEMS):
-            self.menu_win.draw_text(gc, x, 20, label)
+            self.menu_win.draw_text(gc, int(x), 20, str(label))
             x += 80
-        sw = self.root.get_geometry().width
+        sw = int(self.root.get_geometry().width)
         clock_x = sw - 120
-        self.menu_win.draw_text(gc, clock_x, 20, self.clock_text)
+        self.menu_win.draw_text(gc, int(clock_x), 20, str(self.clock_text))
         self.disp.flush()
 
     def update_clock(self):
@@ -98,7 +98,7 @@ class WM:
                 self.handle_menu_click(e)
 
     def handle_menu_click(self, e):
-        x = e.event_x
+        x = int(e.event_x)
         item_width = 80
         for i, (label, cmd) in enumerate(MENU_ITEMS):
             if 10 + i*item_width <= x < 10 + (i+1)*item_width:
@@ -129,8 +129,8 @@ class WM:
         n = len(self.clients)
         if n == 0:
             return
-        sw = self.root.get_geometry().width
-        sh = self.root.get_geometry().height
+        sw = int(self.root.get_geometry().width)
+        sh = int(self.root.get_geometry().height)
         master_w = int(sw * 0.6)
         stack_w = sw - master_w
         for i, win in enumerate(self.clients):
@@ -141,9 +141,9 @@ class WM:
                 win.configure(x=master_w, y=MENU_HEIGHT+(i-1)*h, width=stack_w, height=h, border_width=2)
             if i == self.focused:
                 win.set_input_focus(X.RevertToPointerRoot, X.CurrentTime)
-                win.configure(border_pixel=self.disp.screen().black_pixel)
+                win.configure(border_pixel=int(self.disp.screen().black_pixel))
             else:
-                win.configure(border_pixel=self.disp.screen().white_pixel)
+                win.configure(border_pixel=int(self.disp.screen().white_pixel))
         self.disp.sync()
 
     def handle_key(self, e):
